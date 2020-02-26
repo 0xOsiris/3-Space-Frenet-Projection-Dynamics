78cfe6b4706c1f12205773442e92cfe93ac24bda
Leyton Taylor <leytontaylor23@gmail.com>
2020-02-26 04:39:45 -0600

    Current Functionality and long term goal:
    main method created. xVal, yVal will be defined from a dataset imported that will contain an array(dim=c(2,length(dataset)) with each row
    corresponding to a parametric equation alpha=[x(t),y(t)] that defines a smooth planar curve. Each curve will have 0<=t<=2*pi.
    
    An array of points will be created that correspond to the curvature and torsion of the image of alpha when stereographically projected to the unit sphere from the plane z=-1. beta(alpha(t)) defines the projection of alpha onto the unit sphere. So the array "points" = [curvature(beta(alpha(t)),torsion(beta(alpha(t)) for all t in range(0,2pi)]
    
    "projKTVec"=[function curvature(beta(apha(t)), torsion(beta(alpha(t))]
    
    "projKTVec" can then be passed back into calcOrbits method over a number of iterations and the behavior of the functions can be analyzed graphically by plotting the array "points" corresponding to each the planar curve generated by each iterate of calcOrbits.
    
    Still very much a work in progress.

--- Main.r
+++ Main.r
@@ -1,22 +1,39 @@
 #Author: Leyton Taylor
-
-
+main <-function(){
+  dat <- data.frame(t=seq(0,2*pi,.1))
+  xVal <- function(t) .5*cos(t)
+  yVal <- function(t) .1*sin(t)
+  points<-array( c(), dim=c(2,lenth(dat)))
+  
+  ktVec <- c(xVal,yVal)
+  
+  for(i in seq_along(dat)){
+    projKTVec <- calcOrbits(ktVec)
+    append(points,projKTVec[1:2](dat[i]))
+  }
+  
+}
 #Takes two paramaters that define a parametric function of 
 #parameter t and returns a parametric function that corresponds
 #to the projection of Curvature and Torsion for each t
-calcOrbits <- function(x,y){
-  
-  funVec <- c(expression(x),expression(y),-1)
-  k <- calcProjectionVal(funVec)
-  print(k)
+calcOrbits <- function(ktVec){
+  return(projectionKT(ktVec))
 }
 
 #Returns corresponding curvature(t) and torsion(t)
 #of the projected Parametric function passed to it
-calcProjectionVal <- function(funVec){
-  k_alpha <- calculateCurvature(funVec)
-  return(k_alpha)
+projectionKT <- function(ktVec){
+  #First deriveative of curve [k(t),T(t)]
+  ktVecP= c(D(ktVec[1],'t'), D(ktVec[2],'t'))
+  #Second derivative of curve [k(t),T(t)]
+  ktVecDP = c(D(ktVecP[1],'t'), D(ktVecP[2],'t'))
+  #Vector [gamma(x(t),y(t))]=[Xo(t),Yo(t),Zo(t)] defining a parametric function of the osculating circle of the image of
+  #the planar curve defined by x(t),y(t) at some point t on the unit sphere
+  return(c(curvatureI(ktVec,ktVecP, ktVecDP), torsionI(ktVec,ktVecP,ktVecDP)))
+  
 }
+
+curvatureI(ktVec,ktVecP,ktVecDP)
 
 #Returns the curvature of the parametric curve 
 #Passed to it
@@ -38,11 +55,13 @@ calculateCurvature <- function(funVec){
   return(normCross)
   
 }
+
 #Returns the euclidian norm of a vector v
 calculateNorm <-function(v){
   v <- sqrt(sum(v)^2)
   return(v)
 }
+
 #Returns norm of cross product of two vectors
 calculateCross <- function(funVecP, funVecDP){
   iVal <- funVecP[2]*funVecDP[3]-funVecP[3]*funVecDP[2]
